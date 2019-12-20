bbb265bfdf050c203dc0f48186662ed35caa3131
Dec64 <Declan.h@gmail.com>
2019-12-20 14:50:02 +0000

    Added refresh series before search

--- wantedMissingSonarr.py
+++ wantedMissingSonarr.py
@@ -14,16 +14,15 @@ import time
 # Config
 ################################
 
-host = "https://sonarr.domain.com/"
-api_key = "xxxxxxxxxxxxxxxxxxxxxxx"
+host = "https://sonarr.doamin.com/" # Sonarr URL
+api_key = "xxxxxxxxxxxxxxxxxxx" # Sonarr API Key
 
-sortKey = "airDateUtc"
-page_size = "150"
-page = "1"
-sortDirection = "descending"
-monitored = "true"
+page_size = "150" # Number of episodes to go back and search
+
+refresh = True # Refresh series before performing a search
 
 delay = 10 # timer in seconds between requesting search
+refresh_delay = 5 # timer in seconds to delay between refreshing and searching
 
 ################################
 # Logging
@@ -37,6 +36,10 @@ logger = logging.getLogger(__name__)
 
 headers = {'X-Api-Key': api_key,
            'Content-Type': 'application/json'}
+page = "1" # Do not change
+sortDirection = "descending" # Do not change
+monitored = "true" # Do not change
+sortKey = "airDateUtc" # Do not change
 
 
 ################################
@@ -68,14 +71,32 @@ def search_missing_ep(ep_id):
     r = requests.post(host + 'api/v3/command', headers=headers, timeout=60, json=data)
 
 
+def refresh_series(s_id):
+    data = {"name": "RefreshSeries",
+            "seriesId": s_id}
+    r = requests.post(host + 'api/v3/command', headers=headers, timeout=60, json=data)
+    if r.status_code == 201:
+        return True
+    else:
+        return False
+
+
 missing_episodes = get_missing_list()
 for episode in missing_episodes['records']:
+    refreshed = []
+    if refresh and episode['series']['id'] not in refreshed:
+        print("Refreshing series: {}".format(episode['series']['title']))
+        refresh_series(episode['series']['id'])
+        refreshed.append(episode['series']['id'])
+        print("Waiting {} seconds for refresh".format(refresh_delay))
+        time.sleep(refresh_delay)
     try:
-        print("Forcing scan for {} - {}".format(episode['series']['title'], episode['title']))
+        print("Forcing scan: {} - {}".format(episode['series']['title'], episode['title']))
     except:
         pass
     search_missing_ep(episode['id'])
-    print("waiting {} seconds".format(delay))
+    print("Waiting {} seconds until moving to next episode".format(delay))
+    print("#################")
     time.sleep(delay)
 
 print("#################\n### Finished ###\n#################")
